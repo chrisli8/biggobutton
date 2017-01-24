b850f1de5106543634b3957a2e72da0e17944cda
chrisli8 <8chrisgli@gmail.com>
2017-01-23 16:46:32 -0800

    first files

---
+++ app/src/main/java/biggobutton/chrisli8/washington/edu/biggobutton/MainActivity.java
@@ -0,0 +1,31 @@
+package biggobutton.chrisli8.washington.edu.biggobutton;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.util.Log;
+
+public class MainActivity extends Activity {
+
+    private static int COUNTER = 0;
+    private Button goButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Button goButton = (Button) findViewById(R.id.button_go);
+        goButton.setBackgroundColor(Color.GREEN);
+        goButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                COUNTER++;
+                goButton.setText("You have pushed me " + COUNTER + " times!");
+            }
+        });
+
+    }
+}
